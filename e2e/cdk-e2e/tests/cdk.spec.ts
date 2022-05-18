107503d390bd99f5b81c43f6e047bb1fe6127c1e
James Grom <jgrom@scu.edu>
2022-05-18 11:38:26 -0700

    removed default build generator - updated jest config

--- e2e/cdk-e2e/tests/cdk.spec.ts
+++ e2e/cdk-e2e/tests/cdk.spec.ts
@@ -22,35 +22,65 @@ describe('cdk e2e', () => {
     // some work which can help clean up e2e leftovers
     runNxCommandAsync('reset');
   });
-
-  it('should create cdk', async () => {
-    const project = uniq('cdk');
-    await runNxCommandAsync(`generate @authillo/cdk:cdk ${project}`);
-    const result = await runNxCommandAsync(`build ${project}`);
-    expect(result.stdout).toContain('Executor ran');
+  const project = uniq('cdk');
+  it('should create application', async () => {
+    await runNxCommandAsync(
+      `generate @authillo/cdk:application --projName=${project}`
+    );
   }, 120000);
 
   describe('--directory', () => {
-    it('should create src in the specified directory', async () => {
-      const project = uniq('cdk');
-      await runNxCommandAsync(
-        `generate @authillo/cdk:cdk ${project} --directory subdir`
+    it('should create application', async () => {
+      expect(() => {
+        checkFilesExist(`apps/${project}/src/main.ts`);
+      }).not.toThrow();
+      expect(() => {
+        checkFilesExist(`libs/from${project}/src/index.ts`);
+      }).not.toThrow();
+    });
+  });
+  // describe(`--synth`, () => {
+  //   it('should run synth', async () => {
+  //     const result = await runNxCommandAsync(
+  //       `run ${project}:synth --stackName=${project}`
+  //     );
+  //     expect(result.stdout).toContain(
+  //       'Successfully ran target synth for project'
+  //     );
+  //   }, 120000);
+  // });
+  describe(`--synth:local`, () => {
+    it('should run synth:local', async () => {
+      const result = await runNxCommandAsync(
+        `run ${project}:synth:local --stackName=${project}`
       );
-      expect(() =>
-        checkFilesExist(`libs/subdir/${project}/src/index.ts`)
-      ).not.toThrow();
+      expect(result.stdout).toContain(
+        'Successfully ran target synth for project'
+      );
     }, 120000);
   });
 
-  describe('--tags', () => {
-    it('should add tags to the project', async () => {
-      const projectName = uniq('cdk');
-      ensureNxProject('@authillo/cdk', 'dist/packages/cdk');
-      await runNxCommandAsync(
-        `generate @authillo/cdk:cdk ${projectName} --tags e2etag,e2ePackage`
-      );
-      const project = readJson(`libs/${projectName}/project.json`);
-      expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
-    }, 120000);
-  });
+  // describe('--directory', () => {
+  //   it('should create src in the specified directory', async () => {
+  //     const project = uniq('cdk');
+  //     await runNxCommandAsync(
+  //       `generate @authillo/cdk:cdk ${project} --directory subdir`
+  //     );
+  //     expect(() =>
+  //       checkFilesExist(`libs/subdir/${project}/src/index.ts`)
+  //     ).not.toThrow();
+  //   }, 120000);
+  // });
+
+  // describe('--tags', () => {
+  //   it('should add tags to the project', async () => {
+  //     const projectName = uniq('cdk');
+  //     ensureNxProject('@authillo/cdk', 'dist/packages/cdk');
+  //     await runNxCommandAsync(
+  //       `generate @authillo/cdk:cdk ${projectName} --tags e2etag,e2ePackage`
+  //     );
+  //     const project = readJson(`libs/${projectName}/project.json`);
+  //     expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
+  //   }, 120000);
+  // });
 });
